c3ce46fa0bd69a65dc257d89ebdb94f1dbdeb83a
policelettuce <kartishkovich@mail.ru>
2023-09-16 17:38:54 +0300

    added pic dir cycle

--- predict.py
+++ predict.py
@@ -1,5 +1,13 @@
 from ultralytics import YOLO
+import os
 
-model = YOLO("yolo8_2.pt")
+if __name__ == '__main__':
 
-model.predict(source='1.jpg', show=True, save=True, conf=0.5, line_width=1)
+    model = YOLO("yolo8m_100epoch.pt")
+    dir_path = "D:\!_ITMO_24\HACKATON SAMOLET\TEST_v1\\"        #MAKE SURE TO UPDATE PATH FOR YOUR PC
+
+    for img in os.listdir(dir_path):
+        file_path = dir_path + img
+        print(file_path)
+        print(type(file_path))
+        model.predict(source=file_path, show=False, save=True, conf=0.5, line_width=1)
