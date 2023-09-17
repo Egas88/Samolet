afd26256d0b2b76997cc6f79342e7dbfbd46c9f1
policelettuce <kartishkovich@mail.ru>
2023-09-18 02:15:10 +0300

    added save_txt and save_conf

--- predict.py
+++ predict.py
@@ -3,11 +3,11 @@ import os
 
 if __name__ == '__main__':
 
-    model = YOLO("yolo8m_100epoch.pt")
+    model = YOLO("yolo8m_250epoch.pt")
     dir_path = "D:\!_ITMO_24\HACKATON SAMOLET\TEST_v1\\"        #MAKE SURE TO UPDATE PATH FOR YOUR PC
 
     for img in os.listdir(dir_path):
         file_path = dir_path + img
         print(file_path)
         print(type(file_path))
-        model.predict(source=file_path, show=False, save=True, conf=0.5, line_width=1)
+        model.predict(source=file_path, show=False, save=True, save_conf=True, save_txt=True, conf=0.5, line_width=1)
