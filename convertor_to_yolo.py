a7644718ed1fdb0e8719be39be81b25f4e2bb959
policelettuce <kartishkovich@mail.ru>
2023-09-19 03:27:48 +0300

    fixed finding image obj by its id

--- convertor_to_yolo.py
+++ convertor_to_yolo.py
@@ -1,8 +1,15 @@
 import json
 import os
 
-with open('instances_default.json') as json_file:
+with open('Finisher.json') as json_file:
     data = json.load(json_file)
+
+
+def find_image_by_id(json_data, image_id):
+    for image in json_data['images']:
+        if image['id'] == image_id:
+            return image
+
 
 
 def convert_to_yolo(json_data):
@@ -13,7 +20,7 @@ def convert_to_yolo(json_data):
         category_id = annotation['category_id']
         bbox = annotation['bbox']
 
-        image_info = json_data['images'][image_id - 1]
+        image_info = find_image_by_id(json_data, image_id)
         image_width = image_info['width']
         image_height = image_info['height']
 
@@ -41,8 +48,9 @@ if not os.path.exists("labels"):
 
 # Создание отдельного файла для каждого изображения
 for image_id, annotations in yolo_annotations.items():
-    image_info = data['images'][image_id - 1]
+    image_info = find_image_by_id(data, image_id)
     filename = os.path.join("labels", image_info['file_name'].split('.')[0] + '.txt')
+    print(filename)
 
     with open(filename, 'w') as file:
         for annotation in annotations:
